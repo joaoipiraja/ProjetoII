de3da0c1bc1346dcef89c2ca7bfa27ff8248e150
joaoipiraja <joaovictorpiraja@gmail.com>
2022-11-16 14:29:31 -0300

    Initial Commit

---
+++ ProjetoII/ProjetoIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetoIIApp.swift
+//  ProjetoII
+//
+//  Created by João Victor Ipirajá de Alencar on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ProjetoIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
